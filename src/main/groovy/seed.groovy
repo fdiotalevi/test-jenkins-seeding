d47329a3e00170b01909a729d8787f5e4b3013b0
Filippo Diotalevi <f.diotalevi@orwellg.com>
2018-10-18 17:14:28 +0100

    test seeding jenkins

---
+++ src/main/groovy/seed.groovy
@@ -0,0 +1,29 @@
+import javaposse.jobdsl.dsl.DslFactory
+
+DslFactory factory = this
+
+evaluate(new File("./projects.groovy"))
+
+jobs.each {
+    def name = it["name"]
+    def repo = it["repository"]
+
+    factory.pipelineJob("${name}") {
+        definition {
+
+            cpsScm {
+                scm {
+                    git {
+                        remote {
+                            url(repo)
+                        }
+                        branches("master")
+                        scriptPath("Jenkinsfile")
+                    }
+                }
+            }
+        }
+    }
+}
+
+
