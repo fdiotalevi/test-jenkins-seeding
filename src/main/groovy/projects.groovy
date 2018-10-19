5a8646c1f274fe93b9b3c40feee3f77c1946c04d
Filippo Diotalevi <f.diotalevi@orwellg.com>
2018-10-19 09:52:49 +0100

    update

--- src/main/groovy/projects.groovy
+++ src/main/groovy/projects.groovy
@@ -1,11 +1,2 @@
 
 
-class Jobs {
-    public def jobs =
-            [[
-                     name: "Cryptoeval",
-                     repository: "https://github.com/fdiotalevi/cryptoeval.git"
-             ]]
-
-
-}
--- src/main/groovy/seed.groovy
+++ src/main/groovy/seed.groovy
@@ -3,7 +3,17 @@ import javaposse.jobdsl.dsl.DslFactory
 DslFactory factory = this
 
 
-evaluate(new File("${WORKSPACE}/src/main/groovy/projects.groovy"))
+//evaluate(new File("${WORKSPACE}/src/main/groovy/projects.groovy"))
+
+class Jobs {
+    public def jobs =
+            [[
+                     name: "Cryptoeval",
+                     repository: "https://github.com/fdiotalevi/cryptoeval.git"
+             ]]
+
+
+}
 
 
 new Jobs().jobs.each {
